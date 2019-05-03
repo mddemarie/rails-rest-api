a9c2eda85b887c9e0010d1daf7ae17528d2f5a58
mddemarie <mddemarie@gmail.com>
2019-05-03 18:15:00 +0200

    added the test configuration to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,4 @@
+require 'database_cleaner'
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
@@ -58,4 +59,23 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library
+  end
 end
